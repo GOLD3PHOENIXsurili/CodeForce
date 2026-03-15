c7d60c02c0bd5d4569fb9f59df05e70d95175752
Binu <binaykushwaha1244@gmail.com>
2026-03-16 00:24:32 +0530

    CF 144A Arrival of General

--- 800 Rating/144A_arivalOfGeneral.py
+++ 800 Rating/144A_arivalOfGeneral.py
@@ -1,3 +1,15 @@
 n = int(input())
 a = list(map(int,input().split()))
 
+mx = max(a)
+mn = min(a)
+
+lidx = a.index(mx)
+ridx = len(a) - 1 - a[::-1].index(mn)
+
+moves = lidx + (n-1-ridx)
+
+if lidx > ridx:
+    moves -= 1
+
+print(moves)
